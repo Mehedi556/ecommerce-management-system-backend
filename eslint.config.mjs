caa621b1904ae9cc552fdd2ea46f158904f48968
Abdus Salam Mehedi <salam.mehedi99@gmail.com>
2024-05-22 21:37:31 +0600

    setup express project

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: ['**/node_modules/', '**/dist/'],
+  },
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'no-undef': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+    },
+  },
+  {
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+  },
+);
